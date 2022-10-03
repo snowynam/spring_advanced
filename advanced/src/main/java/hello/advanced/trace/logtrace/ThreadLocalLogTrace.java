386476919da4e6fe08e5a0acb88d3143ab316f07
snowynam <snowynam1@gmail.com>
2022-10-04 00:34:46 +0900

    ThreadLocal

--- advanced/src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
+++ advanced/src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -5,18 +5,19 @@ import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FieldLogTrace implements LogTrace{
+public class ThreadLocalLogTrace implements LogTrace{
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
     
-    private TraceId traceIdHolder;       //traceId 동기화, 동시성 이슈 발생
-    
+    //private TraceId traceIdHolder;       //traceId 동기화, 동시성 이슈 발생
+    private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();       //traceId 동기화, 동시성 이슈 발생
+
     @Override
     public TraceStatus begin(String message) {
         syncTraceId();
-        TraceId traceId = traceIdHolder;
+        TraceId traceId = traceIdHolder.get();
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {} {}",traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         //로그 출력
@@ -24,10 +25,11 @@ public class FieldLogTrace implements LogTrace{
     }
 
     private void syncTraceId() {
-        if(traceIdHolder == null){
-            traceIdHolder = new TraceId();
+        TraceId traceId = traceIdHolder.get();
+        if(traceId == null){
+            traceIdHolder.set(new TraceId());
         } else{
-            traceIdHolder = traceIdHolder.createNextId();
+            traceIdHolder.set(traceId.createNextId());
         }
     }
 
@@ -59,10 +61,11 @@ public class FieldLogTrace implements LogTrace{
     }
 
     private void releaseTraceId() {
-        if(traceIdHolder.isFirstLevel()){
-            traceIdHolder = null;
+        TraceId traceId = traceIdHolder.get();
+        if(traceId.isFirstLevel()){
+            traceIdHolder.remove();     //destroy
         } else {
-            traceIdHolder = traceIdHolder.createPreviousId();
+            traceIdHolder.set(traceId.createPreviousId());
         }
     }
 
