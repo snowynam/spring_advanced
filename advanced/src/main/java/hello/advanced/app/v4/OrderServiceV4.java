e199c03c960c6455e35b6e3d0ee9d2b556c502fa
snowynam <snowynam1@gmail.com>
2022-10-07 00:03:37 +0900

    템플릿메서드 패턴 적용

---
+++ advanced/src/main/java/hello/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,34 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    //생성자
+    /* ctrl+insert constructor
+    public OrderService(OrderRepositoryV0 orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    */
+
+    public void orderItem(String itemId){
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderController.orderItem()");
+
+    }
+}
